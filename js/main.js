e0dd0a2a3ec58669afaf23cd2583ff6e245abac1
Gena <alisxan006@mail.ru>
2020-02-14 19:04:57 +0300

    done

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(function () {
+    $('.menu-btn').click(() => {
+			$(".mobile-menu").toggleClass("show");
+    });
+    $(document).ready(function () {
+			$('.order-btn').magnificPopup({
+				type: 'inline',
+				preloader: false,
+				focus: '#name',
+				callbacks: {
+					beforeOpen: function () {
+						if ($(window).width() < 700) {
+							this.st.focus = false;
+						} else {
+							this.st.focus = '#name';
+						}
+					}
+				}
+			});
+    });
+});
